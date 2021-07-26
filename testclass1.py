cd58adc7832ccc9c5f0400db249d9e35ef133b6f
Nguyễn Đạt <nhdatdat@gmail.com>
2021-07-26 16:36:33 +0900

    first commit

---
+++ testclass1.py
@@ -0,0 +1,11 @@
+class sieu_nhan:
+    pass
+sieu_nhan_A = sieu_nhan()
+
+sieu_nhan_A.ten = "sieu nhan do"
+sieu_nhan_A.vu_khi = "kiem"
+sieu_nhan_A.mausac = "do"
+
+print(f"ten cua sieu nhan A la {sieu_nhan_A.ten}")
+print(f"vu khi cua sieu nhan A la {sieu_nhan_A.vu_khi}")
+print(f"mau sac cua sieu nhan A la {sieu_nhan_A.mausac}")
